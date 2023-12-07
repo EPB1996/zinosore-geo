bfe920a90b74b6fb0c35589ea249f8c7a7034bfa
Etienne Baumgartner <baume96@gmail.com>
2023-12-07 20:14:57 +0100

    map is good

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'map', component: MapComponent },
+  { path: '', redirectTo: 'map', pathMatch: 'full' }, // Add default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
