2f21b015f4282b9a2efad89e1b49d4abf8fc186b
AndraCarla <113671176+AndraCarla@users.noreply.github.com>
2023-12-08 03:19:31 +0100

    feat: window for information

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { Control as LeafletControl } from 'leaflet';
+import {WindowComponent} from "./window/window.component";
+import {MatDialog} from "@angular/material/dialog";
+
 
 @Component({
   selector: 'app-map',
@@ -10,9 +13,11 @@ import { Control as LeafletControl } from 'leaflet';
 export class MapComponent implements OnInit {
   private map: any;
 
-  ngOnInit():void {
-    this.map = L.map('map').setView([46.6031, 1.8904], 5);
+  constructor(private dialog: MatDialog) {
+  }
 
+  ngOnInit(): void {
+    this.map = L.map('map').setView([46.6031, 1.8904], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
@@ -28,13 +33,13 @@ export class MapComponent implements OnInit {
 
     this.map.getContainer().classList.add('grayscale');
 
-    let zoomMaxControl = new LeafletControl({ position: 'topleft' });
+    let zoomMaxControl: L.Control = new LeafletControl({ position: 'topleft' });
 
     zoomMaxControl.onAdd = (map: L.Map) => {
-      let div = L.DomUtil.create('div', 'leaflet-control leaflet-bar leaflet-control-zoom-max');
+      let div: HTMLDivElement = L.DomUtil.create('div', 'leaflet-control leaflet-bar leaflet-control-zoom-max');
       div.innerHTML = '<a href="#" title="Zoom to Max" role="button">Zoom</a>';
 
-      L.DomEvent.on(div, 'click', (e) => {
+      L.DomEvent.on(div, 'click', (e):void => {
         L.DomEvent.preventDefault(e);
         this.zoomToMax();
       });
@@ -42,10 +47,43 @@ export class MapComponent implements OnInit {
     };
 
     zoomMaxControl.addTo(this.map);
-  }
 
+    let information: L.Control = new LeafletControl({ position: 'topleft' });
+
+    information.onAdd = (map: L.Map) => {
+      let div: HTMLDivElement = L.DomUtil.create('div', 'leaflet-control leaflet-bar leaflet-control-zoom-max');
+      div.innerHTML = '<a href="#" title="Information" role="button">Info</a>';
+
+      L.DomEvent.on(div, 'click', (e): void => {
+        L.DomEvent.preventDefault(e);
+        this.showInformation();
+      });
+
+      map.on('click', (e): void => {
+        if (this.dialog.openDialogs.length > 0) {
+          this.dialog.closeAll();
+        }
+      });
+
+      L.DomEvent.disableClickPropagation(div);
+
+      return div;
+    };
+    
+    information.addTo(this.map);
+
+  }
   private zoomToMax():void {
     const minZoom = this.map.getMinZoom();
     this.map.setZoom(minZoom);
   }
+
+  public showInformation(): void {
+    let dialogRef= this.dialog.open(WindowComponent, {
+      width: '250px',
+    });
+    dialogRef.afterClosed().subscribe(result => {})
+  }
+
+
 }
