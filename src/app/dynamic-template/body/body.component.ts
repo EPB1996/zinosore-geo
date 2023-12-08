7092209c10ff96f06316d66bf6713d042e29b7c8
qtambone <quintinobello91@gmail.com>
2023-12-08 05:46:11 +0100

    dynamic template

--- src/app/dynamic-template/body/body.component.ts
+++ src/app/dynamic-template/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-body',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent {
-
+  @Input() public theme : string = '' ;
 }
--- src/app/dynamic-template/dynamic-template.component.ts
+++ src/app/dynamic-template/dynamic-template.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./dynamic-template.component.scss']
 })
 export class DynamicTemplateComponent {
-
+  public theme : string = 'beach' ;
 }
